9251babe2d1d98dc08f25eb6987c2d03478f55c5
Aeonik Chaos <aeonik.chaos@gmail.com>
2023-05-04 17:04:41 -0400

    Trying to add other example code

--- src/gui_interactive.clj
+++ src/gui_interactive.clj
@@ -3,8 +3,16 @@
             [clojure.java.io :as io]
             [clojure.pprint :as pprint]
             [clojure.string :as str]
-            [file-operations :as file-operations])
+            [file-operations :as file-operations]
+            )
   (:import (org.apache.commons.io FileUtils FilenameUtils)))
+
+
+;; you will use custom logic here to determine if it's a prod or dev,
+;; e.g. by using a system property: (Boolean/getBoolean "my-app.dev")
+(def in-development? true)
+(use 'clojure.tools.trace)
+(trace-ns gui-interactive)
 
 ;;TODO remove current-directory from state
 ;;TODO Add the tree to the state
@@ -26,6 +34,18 @@
 ;              (some #(= "dummy" (first %)) (get-in state [::tree id]))
 ;              (update-in [::tree id] #(filter (fn [item] (not= "dummy" (first item))) %)))))
 
+(defmulti handle :event/type)
+
+(defmethod handle ::on-expanded-changed [{:keys [id fx/event]}]
+  (swap!
+    *state
+    #(-> %
+         (update ::expanded (if event conj disj) id)
+         (cond-> (and event (not (get-in % [::tree id])))
+                 (assoc-in [::tree id]
+                           ;; This is "lazy loading". Note: swap! fn might be retried,
+                           ;; don't do side effects here in your app
+                           (random-sample 0.5 (map conj (repeat id) (range 5))))))))
 (defmulti event-handler :event/type)
 
 (defmethod event-handler ::set-friction [e]
@@ -35,17 +55,17 @@
   (println e)
   (swap! *state assoc :gravity (:fx/event e)))
 
-(defmethod event-handler ::on-expanded-changed [e]
-  (println "Entered on-expanded-changed")
-  (let [expanded-path (get e :id)
-        is-expanded?  (get e :fx/event)]
-    (println e)
-    (println expanded-path)
-    (println is-expanded?)
-    (if is-expanded?
-      (swap! *state update :expanded conj expanded-path)
-      (swap! *state update :expanded disj expanded-path)
-        )))
+;(defmethod event-handler ::on-expanded-changed [e]
+;  (println "Entered on-expanded-changed")
+;  (let [expanded-path (get e :id)
+;        is-expanded?  (get e :fx/event)]
+;    (println e)
+;    (println expanded-path)
+;    (println is-expanded?)
+;    (if is-expanded?
+;      (swap! *state update :expanded conj expanded-path)
+;      (swap! *state update :expanded disj expanded-path)
+;        )))
 
 
 ;; For deebugging purposes, print the state to a text box
@@ -84,22 +104,29 @@
   (->> (file-seq (clojure.java.io/file path))
        (filter #(or (.isFile %) (.isDirectory %)))
        (map #(.getCanonicalPath %))))
+
 (take 10 (list-files (System/getProperty "user.home")))
 
-;;TODO - Modify create-file-tree-item to use recur instead
 ;;TODO - Need to make the tree expanded? stateful
+
+(defn is-path-expanded? [file state-atom]
+  (let [canonical-path (.getCanonicalPath file)
+        expanded-paths (get @state-atom :expanded)]
+    (contains? expanded-paths canonical-path)))
+
 (defn create-tree-item
   "Creates a tree item for the given file. Takes the file and a boolean flag expanded? as arguments."
-  [file expanded?]
+  [file]
   (let [canonical-path (.getCanonicalPath file)
-        is-dir         (.isDirectory file)
-        children       (file-operations/subdirs-as-file-objects file)]
+        is-dir         (.isDirectory file)]
     {:fx/type             :tree-item
      :value               (str (FilenameUtils/getBaseName canonical-path))
-     :expanded            expanded?
-     :on-expanded-changed (when is-dir {:event/type ::on-expanded-changed :id canonical-path :expanded expanded?})
-     :children            (when (and is-dir (not expanded?) (seq children))
-                            [{:fx/type :tree-item}])}))
+     :expanded            (is-path-expanded? file *state)
+     :on-expanded-changed (when is-dir {:event/type
+                                        ::on-expanded-changed
+                                        :id canonical-path})}))
+
+
 
 (defn child-tree-items
   "Generates child tree items for the given file.
@@ -107,13 +134,15 @@
   [file]
   (let [is-dir (.isDirectory file)]
     (when is-dir
-      (map #(create-tree-item % false)
+      (map #(create-tree-item %)
            (file-operations/subdirs-as-file-objects file)))))
+
+(child-tree-items (clojure.java.io/file "resources"))
 
 (defn create-file-tree-item
   "Creates a complete file tree item, including children, for the given file."
   [file]
-  (assoc (create-tree-item file true)
+  (assoc (create-tree-item file)
     :children (child-tree-items file)))
 
 
@@ -135,7 +164,7 @@
               {:fx/type :label
                :text    current-directory}
               {:fx/type :tree-view
-               :root    (create-file-tree-item (clojure.java.io/file "resources"))}]})
+               :root    (create-file-tree-item (clojure.java.io/file current-directory))}]})
 
 
 (defn slider-view [{:keys [min max value label event]}]
@@ -150,7 +179,7 @@
                :major-tick-unit  max
                :show-tick-labels true}]})
 
-(defn root-view [{{:keys [gravity friction current-directory expanded]} :state}]
+(defn root-view [{{:keys [gravity friction current-directory expanded tree]} :state}]
   {:fx/type :stage
    :title   "Aeonik's Excellent Adaptation Emporium"
    :showing true
@@ -186,6 +215,22 @@
                                   ]}
              }})
 
+(defn root-view [{::keys [expanded tree]}]
+  (let [->desc (fn ->desc [id]
+                 {:fx/type :tree-item
+                  :value id
+                  :expanded (contains? expanded id)
+                  :on-expanded-changed {:event/type ::on-expanded-changed :id id}
+                  :children (if (or (contains? expanded id) (tree id))
+                              (map ->desc (tree id))
+                              ;; this is a dummy tree item that exists to make
+                              ;; its parent show as expandable:
+                              [{:fx/type :tree-item}])})]
+    {:fx/type :stage
+     :showing true
+     :scene {:fx/type :scene
+             :root {:fx/type :tree-view
+                    :root (->desc [::root])}}}))
 
 (def renderer
   (fx/create-renderer
@@ -194,8 +239,71 @@
                                      :state   state}))
     :opts {:fx.opt/map-event-handler event-handler}))
 
+(def renderer
+  (fx/create-renderer
+    :middleware (fx/wrap-map-desc #'root-view)
+    :opts {:fx.opt/map-event-handler handle}))
+
 (fx/mount-renderer *state renderer)
 
 (renderer)
-(require 'cljfx.dev)
-(cljfx.dev/help-ui)
+;(require 'cljfx.dev)
+;(cljfx.dev/help-ui)
+(defn- ->tree-item [x]
+  (cond
+    (string? x) {:fx/type :tree-item :value x}
+    (seqable? x) {:fx/type :tree-item :value x :children (map ->tree-item x)}
+    :else {:fx/type :tree-item :value x}))
+
+(def tree-table-view
+  {:fx/type :tree-table-view
+   :row-factory {:fx/cell-type :tree-table-row
+                 :describe (fn [x]
+                             {:style {:-fx-background-color (cond
+                                                              (number? x) "#99f"
+                                                              (string? x) "#cfa"
+                                                              (map? x) "fda"
+                                                              (set? x) :pink
+                                                              (coll? x) "#faa"
+                                                              (keyword? x) "eaf"
+                                                              :else "#adf")}})}
+   :columns [{:fx/type :tree-table-column
+              :text "pr-str"
+              :max-width 960/2
+              :cell-value-factory identity
+              :cell-factory {:fx/cell-type :tree-table-cell
+                             :describe (fn [x]
+                                         {:text (pr-str x)})}}
+             {:fx/type :tree-table-column
+              :text "str"
+              :max-width 960/2
+              :cell-value-factory identity
+              :cell-factory {:fx/cell-type :tree-table-cell
+                             :describe (fn [x]
+                                         {:text (str x)})}}]
+   :root (->tree-item
+           {:set #{:a :b :c}
+            :scalars ["string" false 1 1M 1/2 1.0 'symbol :keyword]
+            :map {:a 1}
+            :vec [1 2 3]
+            :list '(1 2 3)
+            :range (range 4)})})
+
+(clojure.tools.trace/trace-form (defn root-view [{{:keys [gravity friction current-directory expanded]} :state}]
+                  {:fx/type :stage
+                   :showing true
+                   :title   "Cell factory examples"
+                   :scene   {:fx/type :scene
+                             :root    {:fx/type     :tab-pane
+                                       :pref-width  960
+                                       :pref-height 540
+                                       :tabs        [
+                                                     {:fx/type  :tab
+                                                      :text     "Tree Table View"
+                                                      :closable false
+                                                      :content  tree-table-view}
+                                                     ]}}}
+
+                  ))
+
+(renderer)
